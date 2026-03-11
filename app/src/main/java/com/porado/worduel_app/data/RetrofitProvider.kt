0545780cd4f0c57e01a3d329b7158e98f51db7d8
sophartncodes <154725881+sophiavitug10@users.noreply.github.com>
2026-03-11 16:43:23 +0800

    new changes: added view models and also modify to connect backend and frontend

--- app/src/main/java/com/porado/worduel_app/data/RetrofitProvider.kt
+++ app/src/main/java/com/porado/worduel_app/data/RetrofitProvider.kt
@@ -1,5 +1,7 @@
 package com.porado.worduel_app.data
 
+import com.porado.worduel_app.data.api.AuthApi
+import com.porado.worduel_app.data.api.UserApi
 import com.porado.worduel_app.data.service.AuthService
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -7,18 +9,28 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitProvider {
 
-    val client = OkHttpClient.Builder()
+    // 1. Define the base URL once
+    private const val BASE_URL = "http://10.0.2.2:8080/"
+
+    // 2. Build the client with the interceptor
+    private val client = OkHttpClient.Builder()
         .addInterceptor { chain ->
             val requestBuilder = chain.request().newBuilder()
-            // Use AuthService.jwtToken, not token
             AuthService.jwtToken?.let { token ->
                 requestBuilder.addHeader("Authorization", "Bearer $token")
             }
             chain.proceed(requestBuilder.build())
         }
         .build()
-    val retrofit = Retrofit.Builder()
-        .baseUrl("http://10.0.2.2:8080/")
+
+    // 3. Build Retrofit AND attach the client
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client) // <-- THIS IS THE CRITICAL ADDITION
         .addConverterFactory(GsonConverterFactory.create())
-        .build();
+        .build()
+
+    // 4. Expose the API implementations to the rest of your app
+    val authApi: AuthApi = retrofit.create(AuthApi::class.java)
+    val userApi: UserApi = retrofit.create(UserApi::class.java)
 }
